06e7817e236e094faf70b8a29d9b4a70337d27ae
Tabu Hana <taboodog@hotmail.com>
2023-03-08 06:09:01 -0800

    updated link amiibo info

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import amiibfo from "../assests/amiibo-info.png";
 import mgl from "../assests/mgl.png"
-import quickCook from "../assests/quick-cook.png";
+// import quickCook from "../assests/quick-cook.png";
 import weatherD from "../assests/weather-dash.png";
 import cryptoFollower from "../assests/crypto-follower.png";
 
@@ -29,7 +29,7 @@ const Projects = () => {
                 Amiibo Locator
               </span>
               <div className='pt-8 text-center'>
-                <a href='https://tabuhana.github.io/amiibo-info/' target="_blank" rel="noopener noreferrer">
+                <a href='https://amiibo-info.vercel.app/' target="_blank" rel="noopener noreferrer">
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                 </a>
                 <a href='https://github.com/TabuHana/amiibo-info' target="_blank" rel="noopener noreferrer">
@@ -79,9 +79,9 @@ const Projects = () => {
 
           {/* Project 4 */}
 
-          <div className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div' style={{ backgroundImage: `url(${quickCook})` }}>
+          {/* <div className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div' style={{ backgroundImage: `url(${quickCook})` }}> */}
             {/* Hover Effects */}
-            <div className='opacity-0 group-hover:opacity-100'>
+            {/* <div className='opacity-0 group-hover:opacity-100'>
               <span className='text-2xl font-bold text-white tracking-wider'>
                 Quick Cook
               </span>
@@ -94,7 +94,7 @@ const Projects = () => {
                 </a>
               </div>
             </div>
-          </div>
+          </div> */}
 
           {/* Project 5 */}
 
