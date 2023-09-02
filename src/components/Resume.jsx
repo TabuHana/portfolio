9b0143016887334fb311d878fbee234acca5d2c3
tabu <taboodog@hotmail.com>
2023-09-02 11:06:29 -0700

    new resume

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,11 +1,12 @@
 import resume from "../assests/nathanielMResume.pdf";
+import resume2 from '../assests/Nathaniel_Marcellous_Resume.pdf'
 
 const Resume = () => {
 
 
   return (
     <div>
-      <iframe className="w-full h-[1100px] bg-[#333] overflow-hidden" name='resume' src={resume} title="resume"></iframe>
+      <iframe className="w-full h-[1100px] bg-[#333] overflow-hidden" name='resume' src={resume2} title="resume"></iframe>
     </div>
   )
 }
